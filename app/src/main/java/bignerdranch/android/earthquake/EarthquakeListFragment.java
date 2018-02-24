73dc4d0d2d6b8d48b857e4574b83213afeaf476a
alieveldar <illuminatiday@gmail.com>
2018-02-24 19:50:40 +0300

    Initial commit

---
+++ app/src/main/java/bignerdranch/android/earthquake/EarthquakeListFragment.java
@@ -0,0 +1,140 @@
+package bignerdranch.android.earthquake;
+
+import android.app.ListFragment;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.DocumentsContract;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+/**
+ * Created by mrx on 2/23/18.
+ */
+
+public class EarthquakeListFragment extends ListFragment {
+    ArrayAdapter<Quake> mQuakeArrayAdapter;
+    ArrayList<Quake> mQuakeArrayList = new ArrayList<Quake>();
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+
+        int layOutId = android.R.layout.simple_list_item_1;
+        mQuakeArrayAdapter = new ArrayAdapter<Quake>(getActivity(), layOutId, mQuakeArrayList);
+        setListAdapter(mQuakeArrayAdapter);
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                refreshEarthQuakes();
+            }
+        });
+        t.start();
+    }
+
+    private static final String  TAG = "EARTQUAKE";
+    private Handler mHandler = new Handler();
+    public void refreshEarthQuakes(){
+        URL url;
+        try {
+            String quakeFeed = getString(R.string.quake_feed);
+            url = new URL(quakeFeed);
+            URLConnection connection;
+            connection = url.openConnection();
+            HttpURLConnection httpURLConnection = (HttpURLConnection)connection;
+            int responseCode = httpURLConnection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK){
+                InputStream in = httpURLConnection.getInputStream();
+
+                DocumentBuilderFactory dbf  = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                Document dom = db.parse(in);
+                Element docEle = dom.getDocumentElement();
+                mQuakeArrayList.clear();
+                NodeList nl = docEle.getElementsByTagName("entry");
+                if (nl != null && nl.getLength() > 0) {
+                    for (int i =0 ; i < nl.getLength(); i++) {
+                        Element entry = (Element)nl.item(i);
+                        Element title = (Element)entry.getElementsByTagName("title").item(0);
+                        Element g =(Element)entry.getElementsByTagName("georss:point").item(0);
+                        Element when = (Element)entry.getElementsByTagName("updated").item(0);
+                        Element link = (Element)entry.getElementsByTagName("link").item(0);
+
+                        String details = title.getFirstChild().getNodeValue();
+                        String hostname = "http://earthquake.usgs.goov";
+                        String linkString = hostname + link.getAttribute("href");
+
+                        String point = g.getFirstChild().getNodeValue();
+                        String dt = when.getFirstChild().getNodeValue();
+                        SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd'T'hh:mm:ss'Z'");
+                        Date qdate = new GregorianCalendar(0,0,0).getTime();
+                        try {
+                            qdate = sdf.parse(dt);
+
+                        }catch (ParseException e) {
+                            Log.d(TAG, "Dateparsing exception", e);
+                        }
+
+                        String[] location = point.split(" ");
+                        Location l = new Location("dummyGPS");
+                        l.setLatitude(Double.parseDouble(location[0]));
+                        l.setLongitude(Double.parseDouble(location[1]));
+
+                        String magnitudeString = details.split(" ")[1];
+                        int end =magnitudeString.length() -1 ;
+                        Double magnitude = Double.parseDouble(magnitudeString.substring(0, end));
+                        details = details.split(",")[1].trim();
+                        final Quake quake = new Quake(qdate, details, l,  magnitude, linkString);
+
+                        mHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                addNewQuake(quake);
+                            }
+                        });
+
+                    }
+                }
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+        finally {
+
+        }
+    }
+    private void addNewQuake(Quake _quake) {
+        mQuakeArrayList.add(_quake);
+        mQuakeArrayAdapter.notifyDataSetChanged();
+    }
+
+}
---
+++ app/src/main/java/bignerdranch/android/earthquake/Quake.java
@@ -0,0 +1,54 @@
+package bignerdranch.android.earthquake;
+
+import android.icu.text.SimpleDateFormat;
+import android.location.Location;
+
+import java.util.Date;
+
+/**
+ * Created by mrx on 2/24/18.
+ */
+
+public class Quake {
+    private Date mDate;
+    private String detalis;
+    private Location mLocation;
+    private double magnitude;
+    private String link;
+
+    public Quake(Date date, String detalis, Location location, double magnitude, String link) {
+        mDate = date;
+        this.detalis = detalis;
+        mLocation = location;
+        this.magnitude = magnitude;
+        this.link = link;
+    }
+
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public String getDetalis() {
+        return detalis;
+    }
+
+    public Location getLocation() {
+        return mLocation;
+    }
+
+    public double getMagnitude() {
+        return magnitude;
+    }
+
+    public String getLink() {
+        return link;
+    }
+    @Override
+    public String toString(){
+        SimpleDateFormat sdf = new SimpleDateFormat("HH.mm");
+        String dateString = sdf.format(mDate);
+        return dateString + ": " + magnitude + " " + detalis;
+
+    }
+}
