8fc210ee28970c7c6941820a4301be9ff9cf1a18
alieveldar <illuminatiday@gmail.com>
2018-03-08 11:46:24 +0300

    page 339 Adding Search in app. But it not work

--- app/src/main/java/bignerdranch/android/earthquake/EarthQuakeSearchResullts.java
+++ app/src/main/java/bignerdranch/android/earthquake/EarthQuakeSearchResullts.java
@@ -2,6 +2,8 @@ package bignerdranch.android.earthquake;
 
 import android.app.ListActivity;
 import android.app.LoaderManager;
+import android.app.SearchManager;
+import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
@@ -15,6 +17,7 @@ import android.widget.SimpleCursorAdapter;
 public class EarthQuakeSearchResullts extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private SimpleCursorAdapter adapter;
+    private static final String QUERY_EXTRA_KEY = "QUERY_EXTRA_KEY";
     @Override
     public void onCreate(Bundle savedInstanceState){
         adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, null,
@@ -26,17 +29,28 @@ public class EarthQuakeSearchResullts extends ListActivity implements LoaderMana
     }
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return null;
+        String query = "0";
+
+        if (args != null) {
+            query = args.getString(QUERY_EXTRA_KEY);
+        }
+            String[] projection = {EarthquakeProvider.KEY_ID, EarthquakeProvider.KEY_SUMMARY};
+            String where = EarthquakeProvider.KEY_SUMMARY + " LIKE " + query;
+            String[] whereArgs = null;
+            String sortOrder = EarthquakeProvider.KEY_SUMMARY + " COLLATE LOCALIZED ASC";
+
+            return new CursorLoader(this, EarthquakeProvider.CONTENT_URI, projection, where, whereArgs, sortOrder);
+
     }
 
     @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        adapter.swapCursor(cursor);
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-
+        adapter.swapCursor(null);
     }
 
     @Override
@@ -45,6 +59,11 @@ public class EarthQuakeSearchResullts extends ListActivity implements LoaderMana
         parseIntent(getIntent());
     }
     private void parseIntent(Intent intent){
-
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())){
+            String searchQuery = intent.getStringExtra(SearchManager.QUERY);
+            Bundle args =new Bundle();
+            args.putString(QUERY_EXTRA_KEY, searchQuery);
+            getLoaderManager().restartLoader(0, args, this);
+        }
     }
 }
