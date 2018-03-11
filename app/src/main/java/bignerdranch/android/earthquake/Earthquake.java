fbceed8286965925f0e37bef609115306d889d45
alieveldar <illuminatiday@gmail.com>
2018-03-11 19:06:27 +0300

    page 397 Layout

--- app/src/main/java/bignerdranch/android/earthquake/Earthquake.java
+++ app/src/main/java/bignerdranch/android/earthquake/Earthquake.java
@@ -1,50 +1,77 @@
 package bignerdranch.android.earthquake;
 
+import android.app.ActionBar;
 import android.app.Activity;
-import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.app.SearchManager;
 import android.app.SearchableInfo;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Resources;
 import android.os.Build;
+import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.SearchView;
 
+;
+
 public class Earthquake extends AppCompatActivity {
-static final private int MENU_PREFERENCES = Menu.FIRST + 1;
-static final private int MENU_UPDATE = Menu.FIRST + 2;
-private static final int SHOW_PREFERENCES = 1;
+    static final private int MENU_PREFERENCES = Menu.FIRST + 1;
+    static final private int MENU_UPDATE = Menu.FIRST + 2;
+    private static final int SHOW_PREFERENCES = 1;
 
     public int minimumMagnitude = 0;
     public boolean autoUpdateChecked = false;
     public int updateFreq = 0;
 
-@Override
-public boolean onCreateOptionsMenu(Menu menu){
-    super.onCreateOptionsMenu(menu);
-    menu.add(0, MENU_PREFERENCES, Menu.NONE, R.string.menu_preferences);
-    return true;
-}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0, MENU_PREFERENCES, Menu.NONE, R.string.menu_preferences);
+        return true;
+    }
+    TabListener<EarthquakeListFragment> listTabListener;
+    TabListener<EarthquakeMapFragment> mapTabListener;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         updateFromPreferences();
-        SearchManager searchManager = (SearchManager)getSystemService(Context.SEARCH_SERVICE);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchableInfo searchableInfo = searchManager.getSearchableInfo(getComponentName());
 
-        SearchView searchView = (SearchView)findViewById(R.id.searchView);
+        SearchView searchView = (SearchView) findViewById(R.id.searchView);
         searchView.setSearchableInfo(searchableInfo);
+
+        ActionBar actionBar = getActionBar();
+
+        View fragmentContainer = findViewById(R.id.EarthquakeFragmentContainer);
+
+        boolean tabletLayout = fragmentContainer == null;
+
+        if (!tabletLayout){
+            actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+            actionBar.setDisplayShowTitleEnabled(false);
+
+            android.app.ActionBar.Tab listTab = actionBar.newTab();
+
+            listTabListener = new TabListener<EarthquakeListFragment>(this, R.id.EarthquakeFragmentContainer, EarthquakeListFragment.class);
+            listTab.setText("List").setContentDescription("List of earthquakes").setTabListener(listTabListener);
+            actionBar.addTab(listTab);
+
+            ActionBar.Tab mapTab = actionBar.newTab();
+
+            mapTabListener = new TabListener<EarthquakeMapFragment>(this, R.id.EarthquakeMapFragment, EarthquakeMapFragment.class);
+            mapTab.setText("Map").setContentDescription("Map of earthquakes").setTabListener(mapTabListener);
+            actionBar.addTab(mapTab);
+        }
+
     }
-
-
 
 
     @Override
@@ -63,7 +90,7 @@ public boolean onCreateOptionsMenu(Menu menu){
         return false;
     }
 
-    private void updateFromPreferences(){
+    private void updateFromPreferences() {
         Context context = getApplicationContext();
         SharedPreferences spref = PreferenceManager.getDefaultSharedPreferences(context);
 
@@ -72,26 +99,53 @@ public boolean onCreateOptionsMenu(Menu menu){
         autoUpdateChecked = spref.getBoolean(PreferencesActivity.PREF_AUTO_UPDDATE, false);
 
 
-
     }
 
     @Override
-    public void onActivityResult(int requestCode, int rezultCode, Intent  data ){
+    public void onActivityResult(int requestCode, int rezultCode, Intent data) {
         super.onActivityResult(requestCode, requestCode, data);
-        if (requestCode == SHOW_PREFERENCES)
+        if (requestCode == SHOW_PREFERENCES) {
             updateFromPreferences();
+            startService(new Intent(this, EarthquakeService.class));
 
-                FragmentManager fm = getFragmentManager();
-                final EarthquakeListFragment earthQuakeList = (EarthquakeListFragment)fm.findFragmentById(R.id.Earthquakelistfragment);
-
-                Thread t = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        earthQuakeList.refreshEarthQuakes();
-                    }
-                });
-                t.start();
+        }
 
     }
 
+    private static String ACTION_BAR_INDEX = "ACTION_BAR_INDEX";
+
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        View fragmentConatainer = findViewById(R.id.EarthquakeFragmentContainer);
+        boolean tabletLayout = fragmentConatainer == null;
+
+        if (!tabletLayout){
+            int actionBarIndex = getActionBar().getSelectedTab().getPosition();
+            SharedPreferences.Editor editor = getPreferences(Activity.MODE_PRIVATE).edit();
+            editor.putInt(ACTION_BAR_INDEX, actionBarIndex);
+            editor.apply();
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            if (mapTabListener.fragment != null)
+                ft.detach(mapTabListener.fragment);
+            if (listTabListener.fragment != null)
+                ft.detach(listTabListener.fragment);
+            ft.commit();
+
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+
+        View fragmentContainer = findViewById(R.id.EarthquakeFragmentContainer);
+        boolean tabletLayout = fragmentContainer == null;
+
+        if (!tabletLayout){
+            SharedPreferences sp = getPreferences(Activity.MODE_PRIVATE);
+            int actionBarIndex =  sp.getInt(ACTION_BAR_INDEX, 0);
+            getActionBar().setSelectedNavigationItem(actionBarIndex);
+        }
+    }
 }
--- app/src/main/java/bignerdranch/android/earthquake/EarthquakeListFragment.java
+++ app/src/main/java/bignerdranch/android/earthquake/EarthquakeListFragment.java
@@ -1,48 +1,19 @@
 package bignerdranch.android.earthquake;
 
-import android.app.ListFragment;
-import android.app.LoaderManager;
-import android.content.ContentResolver;
-import android.content.ContentValues;
-import android.content.CursorLoader;
+//import android.content.CursorLoader;
+
 import android.content.Intent;
-import android.content.Loader;
 import android.database.Cursor;
-import android.location.Location;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.provider.DocumentsContract;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v4.widget.SimpleCursorAdapter;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.GregorianCalendar;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 /**
  * Created by mrx on 2/23/18.
  */
 
-public class EarthquakeListFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor> {
+public class EarthquakeListFragment extends android.support.v4.app.ListFragment implements android.support.v4.app.LoaderManager.LoaderCallbacks<Cursor> {
 
     SimpleCursorAdapter adapter;
 
@@ -77,19 +48,20 @@ public class EarthquakeListFragment extends ListFragment implements LoaderManage
         String where = EarthquakeProvider.KEY_MAGNITUDE + " > " +
                 earthquakeActivvity.minimumMagnitude;
 
-        CursorLoader loader = new CursorLoader(getActivity(), EarthquakeProvider.CONTENT_URI, projection, where, null,null);
+        Loader<Cursor> loader = new CursorLoader(getActivity(), EarthquakeProvider.CONTENT_URI, projection, where, null,null);
         return loader;
     }
 
     @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+    public void onLoadFinished(android.support.v4.content.Loader<Cursor> loader, Cursor cursor) {
         adapter.swapCursor(cursor);
     }
 
     @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
+    public void onLoaderReset(android.support.v4.content.Loader<Cursor> loader) {
         adapter.swapCursor(null);
     }
+
 
     public void refreshEarthQuakes(){
         getLoaderManager().restartLoader(0,null,EarthquakeListFragment.this);
